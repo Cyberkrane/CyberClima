cd07a8e0bafaeb5dab7df8bb02f759a650407684
Cyberkrane <cyberkrane@gmail.com>
2022-06-27 18:39:47 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'cyberclima_version_1.6.0';
+}
