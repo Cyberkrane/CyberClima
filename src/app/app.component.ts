f04d863d9a9653277963c5348c812d31b25cf2a2
Cyberkrane <cyberkrane@gmail.com>
2022-06-28 01:01:53 -0300

    mi ubicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
 import { Component } from '@angular/core';
+import { ClimaService } from './services/clima.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cyberclima_version_1.6.0';
+
+  clima: any;
+  mi_ubicacion = localStorage.getItem('miUbicacion');
+  nombre_de_la_ciudad: any = this.mi_ubicacion;
+  formulario: any = document.getElementById('form');
+
+  constructor(private climaService: ClimaService) {}
+
+  ngOnInit(): void {
+
+    if (this.mi_ubicacion) {
+      this.getClima(this.mi_ubicacion);
+      {
+        this.climaService.getClima(this.mi_ubicacion).subscribe(
+          (resp) => {
+            (this.clima = resp), console.log(this.clima);
+          },
+          (err) => console.log(err)
+        );
+      }
+  
+    } 
+  }
+
+  getClima(nombre_de_la_ciudad: string) {
+    this.climaService.getClima(nombre_de_la_ciudad).subscribe(
+      (resp) => {
+        (this.clima = resp), console.log(this.clima);
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  enviarUbicaion(nombre_de_la_ciudad: HTMLInputElement) {
+    if (nombre_de_la_ciudad.value) {
+      this.getClima(nombre_de_la_ciudad.value);
+
+      nombre_de_la_ciudad.value = '';
+    } else {
+      alert('por favor ingrese nombre de ciudad y pais');
+    }
+
+    nombre_de_la_ciudad.focus();
+
+    return false;
+  }
+
+  miUbicaion(nombre_de_la_ciudad: HTMLInputElement) {
+    if (nombre_de_la_ciudad.value) {
+      this.getClima(nombre_de_la_ciudad.value);
+
+      localStorage.setItem('miUbicacion', nombre_de_la_ciudad.value);
+      nombre_de_la_ciudad.value = '';
+    } else {
+      alert('por favor ingrese nombre de ciudad y pais');
+    }
+
+    nombre_de_la_ciudad.focus();
+
+    return false;
+  }
 }
---
+++ src/app/services/clima.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClimaService {
+
+  apiKey: string = 'ad4ea803fef9fc30a55a98365239c1f9'
+  URI:    string = ''
+  nombreDeCiudad: string = ''
+  
+
+  constructor(private http: HttpClient) { 
+    this.URI = `https://api.openweathermap.org/data/2.5/weather?&appid=${this.apiKey}&units=metric&lang=sp&q=`
+  }
+// , codigoDePais: string
+  getClima(nombreDeCiudad: string){
+
+    // return this.http.get(`${this.URI}${nombreDeCiudad}, ${codigoDePais}`)
+
+    return this.http.get(`${this.URI}${nombreDeCiudad}`)
+  
+  }
+}
