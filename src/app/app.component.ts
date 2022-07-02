69a164bd7524fa320c97dd1b93ce224641ec5014
Cyberkrane <cyberkrane@gmail.com>
2022-07-02 17:49:18 -0300

    up6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { ClimaService } from './services/clima.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cyberclima_version_1.6.0';
@@ -13,7 +13,6 @@ export class AppComponent {
   mi_ubicacion = localStorage.getItem('miUbicacion');
   nombre_de_la_ciudad: any = this.mi_ubicacion;
   hora: number = 12;
-  
 
   constructor(private climaService: ClimaService) {}
 
@@ -22,7 +21,8 @@ export class AppComponent {
       this.getClima(this.mi_ubicacion);
     }
     this.hora = new Date().getHours();
-    console.log(this.hora)
+
+ 
   }
 
   getClima(nombre_de_la_ciudad: string) {
@@ -30,7 +30,9 @@ export class AppComponent {
       (resp) => {
         this.clima = resp;
       },
-      (err) => { console.log(err) }
+      (err) => {
+        console.log(err);
+      }
     );
   }
 
@@ -39,7 +41,6 @@ export class AppComponent {
       this.getClima(nombre_de_la_ciudad.value);
       localStorage.setItem('miUbicacion', nombre_de_la_ciudad.value);
       nombre_de_la_ciudad.value = '';
-      
     } else {
       alert('por favor ingrese nombre de ciudad');
     }
@@ -47,5 +48,27 @@ export class AppComponent {
     nombre_de_la_ciudad.focus();
 
     return false;
+  }
+
+  enVozAlta() {
+    console.log('hablando........');
+
+    const boton = document.querySelector('#btnEscuchar');
+    const $mensaje = document.querySelector('#mensaje')?.innerHTML;
+    let vocesDisponibles = [];
+
+    // Si hay evento, entonces lo esperamos
+    if ('onvoiceschanged' in speechSynthesis) {
+      speechSynthesis.onvoiceschanged = function () {
+        vocesDisponibles = speechSynthesis.getVoices();
+        let textoAEscuchar = $mensaje!;
+        let mensaje = new SpeechSynthesisUtterance();
+        mensaje.voice = vocesDisponibles[13];
+        mensaje.volume = 1;
+        mensaje.text = textoAEscuchar;
+        mensaje.pitch = 1;
+        speechSynthesis.speak(mensaje);
+      };
+    }
   }
 }
--- src/assets/voice.js
+++ src/assets/voice.js
@@ -5,29 +5,21 @@ document.addEventListener("click", () => {
     $mensaje = document.querySelector("#mensaje");
    vocesDisponibles = [];
 
-  // Función que pone las voces dentro del select
-  const cargarVoces = () => {
-    vocesDisponibles = speechSynthesis.getVoices();
-  };
-
   // Si no existe la API, lo indicamos
   if (!'speechSynthesis' in window) return alert("Lo siento, tu navegador no soporta esta tecnología");
 
   // Si hay evento, entonces lo esperamos
-  // if ('onvoiceschanged' in speechSynthesis) {
-    // speechSynthesis.onvoiceschanged = function () {
-    //   cargarVoces();
-    // };
-  // }
-
-  // El click del botón. Aquí sucede la magia
-  $boton.addEventListener("click", () => {
+ if ('onvoiceschanged' in speechSynthesis) {
+    speechSynthesis.onvoiceschanged = function () {
+    vocesDisponibles = speechSynthesis.getVoices();
     let textoAEscuchar = $mensaje.value;
     let mensaje = new SpeechSynthesisUtterance();
     mensaje.voice = vocesDisponibles[13];
     mensaje.volume = 1;
     mensaje.text = textoAEscuchar;
-    mensaje.pitch = 0.8;
+    mensaje.pitch = 1;
     speechSynthesis.speak(mensaje);
-  });
+    };
+   }
+
 });
