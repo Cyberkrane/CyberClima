4fa8b9c23016e913dfd79c03fe8e0083d371e560
Cyberkrane <cyberkrane@gmail.com>
2022-07-02 01:47:08 -0300

    se eliminaron imagenes no utilizadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { ClimaService } from './services/clima.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'cyberclima_version_1.6.0';
---
+++ src/assets/voice.js
@@ -0,0 +1,33 @@
+// Esperar a que el que DOM cargue
+document.addEventListener("click", () => {
+  const $voces = document.querySelector("#voces"),
+    $boton = document.querySelector("#btnEscuchar"),
+    $mensaje = document.querySelector("#mensaje");
+   vocesDisponibles = [];
+
+  // Función que pone las voces dentro del select
+  const cargarVoces = () => {
+    vocesDisponibles = speechSynthesis.getVoices();
+  };
+
+  // Si no existe la API, lo indicamos
+  if (!'speechSynthesis' in window) return alert("Lo siento, tu navegador no soporta esta tecnología");
+
+  // Si hay evento, entonces lo esperamos
+  // if ('onvoiceschanged' in speechSynthesis) {
+    speechSynthesis.onvoiceschanged = function () {
+      cargarVoces();
+    };
+  // }
+
+  // El click del botón. Aquí sucede la magia
+  $boton.addEventListener("click", () => {
+    let textoAEscuchar = $mensaje.value;
+    let mensaje = new SpeechSynthesisUtterance();
+    mensaje.voice = vocesDisponibles[$voces.value];
+    mensaje.volume = 1;
+    mensaje.text = textoAEscuchar;
+    mensaje.pitch = 0.5;
+    speechSynthesis.speak(mensaje);
+  });
+});
